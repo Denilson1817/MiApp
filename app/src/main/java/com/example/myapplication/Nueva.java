ada1430c2d7ec73ab10b946400cb3cc7a4763e18
jorge <arbjorfuje@gmail.com>
2023-04-27 20:23:45 -0600

    Pruebas50

--- app/src/main/java/com/example/myapplication/Nueva.java
+++ app/src/main/java/com/example/myapplication/Nueva.java
@@ -1,4 +1,22 @@
 package com.example.myapplication;
 
 public class Nueva {
+    Integer num;
+    String dia="";
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
 }
