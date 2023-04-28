3525ef116591645bd2cd798275975bb154bdf911
denilson1817 <denilson_peco@hotmail.com>
2023-04-28 14:56:58 -0600

    add database

--- app/src/main/java/com/example/myapplication/PeliculasActivity.java
+++ app/src/main/java/com/example/myapplication/PeliculasActivity.java
@@ -3,12 +3,41 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class PeliculasActivity extends AppCompatActivity {
-
+    Button btnGuardarPeliculas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_peliculas);
+        btnGuardarPeliculas = findViewById(R.id.btnGuardarPelicula);
+
+        btnGuardarPeliculas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //recuperamos datos del edit    Text titulo
+                EditText etxtTitulop = (EditText) findViewById(R.id.txtTituloPelicula);
+                String titulo = etxtTitulop.getText().toString();
+
+                //recuperamos datos del spiner
+                Spinner spGeneroPelicula =(Spinner) findViewById(R.id.generosp);
+                String genero = spGeneroPelicula.getSelectedItem().toString();
+                //crear objeto pelicula
+                Pelicula pelicula = new Pelicula(titulo,genero);
+                //isntancia aa una referencia a firebase
+                DatabaseReference miDRef = FirebaseDatabase.getInstance().getReference().child("peliculas");
+                //agregar una pelicula utilizando el metodo push paraa genrar una clave unica
+                DatabaseReference nuevaPelicula = miDRef.push();
+                nuevaPelicula.setValue(pelicula);
+            }
+        });
     }
 }
