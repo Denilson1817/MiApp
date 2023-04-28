6f46210e9d35d0fa6d99bc44774821abf01773b0
jorge <arbjorfuje@gmail.com>
2023-04-27 19:34:38 -0600

    Pruebas

--- app/src/main/java/com/example/myapplication/Cagadas.java
+++ app/src/main/java/com/example/myapplication/Cagadas.java
@@ -4,4 +4,29 @@ public class Cagadas {
     String Denilson="";
     Integer dia;
     String hola="cagada javi";
+
+
+    public String getDenilson() {
+        return Denilson;
+    }
+
+    public void setDenilson(String denilson) {
+        Denilson = denilson;
+    }
+
+    public Integer getDia() {
+        return dia;
+    }
+
+    public void setDia(Integer dia) {
+        this.dia = dia;
+    }
+
+    public String getHola() {
+        return hola;
+    }
+
+    public void setHola(String hola) {
+        this.hola = hola;
+    }
 }
